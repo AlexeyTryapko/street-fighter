1f2919a54801442337af8801125387b8a8a342b3
Алексей Тряпко <aatryapko@mail.ru>
2019-05-20 12:25:24 +0300

    Improved createElement method

--- src/javascript/view.js
+++ src/javascript/view.js
@@ -1,9 +1,9 @@
 class View {
   element;
 
-  createElement({ tagName, className = '', attributes = {} }) {
+  createElement({ tagName, classNames = null, attributes = {} }) {
     const element = document.createElement(tagName);
-    if(className) element.classList.add(className);
+    if(classNames) element.classList.add(...classNames);
     Object.keys(attributes).forEach(key => element.setAttribute(key, attributes[key]));
 
     return element;
